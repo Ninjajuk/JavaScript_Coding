cf11b2197ddc665e2bc5c23839a5070a07f7ed52
Samsu <biosamsuddin@gmail.com>
2023-12-05 17:32:26 +0530

    array done at office

---
+++ Array/array.js
@@ -0,0 +1,24 @@
+
+Array.prototype.sam='Samsu'
+
+const num=[1,2,3,4,5]
+for (let x in num){
+    console.log(x)
+}
+const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
+
+const mixedArray = [
+    { name: 'John', age: 25, city: 'New York', isAdmin: false },
+    { title: 'JavaScript Basics', pages: 150, isPublished: true, author: 'Alice' },
+    { country: 'France', population: 67000000, capital: 'Paris', landmarks: ['Eiffel Tower', 'Louvre Museum'] },
+    { person: { firstName: 'Bob', lastName: 'Smith' }, job: 'Software Developer', experience: 3 },
+    { color: 'Blue', hexCode: '#0000FF', rgb: [0, 0, 255] },
+    { fruit: 'Apple', quantity: 5, isFresh: true },
+    { temperature: 28.5, unit: 'Celsius', isHot: true },
+    { car: 'Toyota', model: 'Camry', year: 2022, isHybrid: false },
+    { isActive: true, dateJoined: new Date('2023-01-15') },
+    { student: 'Mary', grades: { math: 90, science: 85, history: 92 } }
+  ];
+  
+  console.log(mixedArray);
+  
