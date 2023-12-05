197441406761f7f1016ab0546484b400c8b77c03
Samsu <biosamsuddin@gmail.com>
2023-12-05 09:37:31 +0530

    promis

--- Stock/PercentageChange.js
+++ Stock/PercentageChange.js
@@ -6,21 +6,19 @@ function calculateChangeArray(oldValues, newValues) {
     for (let i = 0; i < oldValues.length; i++) {
         const oldValue = oldValues[i];
         const newValue = newValues[i];
-
         const absoluteChange = newValue - oldValue;
-        const percentageChange = (oldValue === 0) ? "N/A" : ((absoluteChange / Math.abs(oldValue)) * 100).toFixed(2) + "%";
+        const percentageChange = (oldValue === 0) ? "N/A" : ((absoluteChange / Math.round(oldValue)) * 100).toFixed(2) + "%";
 
         changes.push({
             absoluteChange: absoluteChange,
             percentageChange: percentageChange
         });
     }
-
     return changes;
 }
 // Example data
-const oldValues = [100, 120, 130, 110, 150];  // initial values
-const newValues = [120, 130, 110, 150, 180];  // final values
+const oldValues = [342.80, 120, 130, 110, 150];  // initial values
+const newValues = [401.75, 130, 110, 150, 180];  // final values
 
 // Calculate changes
 const changes = calculateChangeArray(oldValues, newValues);
@@ -29,7 +27,7 @@ const changes = calculateChangeArray(oldValues, newValues);
 for (let i = 0; i < changes.length; i++) {
     console.log(`Change for pair ${i + 1}:`);
     console.log(`  Absolute Change: ${changes[i].absoluteChange}`);
-    console.log(`  Percentage Change: ${changes[i].percentageChange}`);
+    console.log(`  Return: ${changes[i].percentageChange}`);
     console.log();
 }
 
---
+++ Stock/dailyReturns.js
@@ -0,0 +1,14 @@
+
+// Return: 
+
+// Daily return: =(Today’s closing price - Previous day’s closing price) ÷ Previous day’s closing price
+
+// Weekly return: =(Today’s closing price - Previous day’s closing price) ÷ Previous day’s closing price
+
+// Monthly return: =(Today’s closing price - Previous day’s closing price) ÷ Previous day’s closing price
+
+// 3 Monthly return: =(Today’s closing price - Previous day’s closing price) ÷ Previous day’s closing price
+
+// 6 Monthly return: =(Today’s closing price - Previous day’s closing price) ÷ Previous day’s closing price
+
+
