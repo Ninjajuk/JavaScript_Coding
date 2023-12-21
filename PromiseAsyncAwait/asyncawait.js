110dee90290500c1f8e84ff02732cd96bb344c1d
SAMSUDDIN ANSARI <108830220+Ninjajuk@users.noreply.github.com>
2023-12-22 00:55:40 +0530

    Update asyncawait.js

--- PromiseAsyncAwait/asyncawait.js
+++ PromiseAsyncAwait/asyncawait.js
@@ -1,1 +1,12 @@
+//Async and await makes easy to handle promise.
 
+//Example 
+
+async function getData(){
+  const data=await fetch('https://dummyjson.com/products/1')
+  const resp=await data.json()
+  console.log(resp)
+    return resp
+  
+}
+getData()
