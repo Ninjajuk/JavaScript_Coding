55aeb1a8327cfb86cef338282fc3d5b58d699817
Samsu <biosamsuddin@gmail.com>
2024-02-01 16:59:15 +0530

    find

--- Array/array.js
+++ Array/array.js
@@ -63,3 +63,5 @@ const array3 = array1.concat(array2);
 
 console.log(array3);
 // Expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+//find()=>The find() method of Array instances returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
