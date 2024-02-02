042e51e1cb95ea7cf390ff821df0657023a3d63d
Samsu <biosamsuddin@gmail.com>
2024-02-02 17:41:13 +0530

    async

--- PromiseAsyncAwait/asyncawait.js
+++ PromiseAsyncAwait/asyncawait.js
@@ -10,10 +10,71 @@ async function getData(){
 }
 getData()
 
-async function getUser(){
-  const data=await fetch('https://dummyjson.com/users/')
-  const resp=await data.json()
-  console.log(resp)
-    return resp 
+// async function getUser(){
+//   const data=await fetch('https://dummyjson.com/users/')
+//   const resp=await data.json()
+//   console.log(resp)
+//     return resp 
+// }
+// getUser()
+
+
+function getUser(userId) {
+  return new Promise((resolve, reject) => {
+      console.log('Get user from the database.');
+      setTimeout(() => {
+          resolve({
+              userId: userId,
+              username: 'john'
+          });
+      }, 1000);
+  })
 }
-getUser()
+
+function getServices(user) {
+  return new Promise((resolve, reject) => {
+      console.log(`Get services of  ${user.username} from the API.`);
+      setTimeout(() => {
+          resolve(['Email', 'VPN', 'CDN']);
+      }, 2 * 1000);
+  });
+}
+
+function getServiceCost(services) {
+  return new Promise((resolve, reject) => {
+      console.log(`Calculate service costs of ${services}.`);
+      setTimeout(() => {
+          resolve(services.length * 100);
+      }, 3 * 1000);
+  });
+}
+getUser(100)
+    .then(getServices)
+    .then(getServiceCost)
+    .then(console.log);
+async function getUser(userId) {
+  try {
+     const user = await Promise.reject(new Error('Invalid User Id'));
+  } catch(error) {
+     console.log(error);
+  }
+}
+
+async function showServiceCost() {
+  let user = await getUser(100);
+  let services = await getServices(user);
+  let cost = await getServiceCost(services);
+  console.log(`The service cost is ${cost}`);
+}
+
+showServiceCost();
+async function showServiceCost() {
+  try {
+     let user = await getUser(100);
+     let services = await getServices(user);
+     let cost = await getServiceCost(services);
+     console.log(`The service cost is ${cost}`);
+  } catch(error) {
+     console.log(error);
+  }
+}
