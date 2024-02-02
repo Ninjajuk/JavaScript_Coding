e2dd2fad61cb38b92d7061112feec9e001d4a358
Samsu <biosamsuddin@gmail.com>
2024-02-02 17:35:10 +0530

    l

--- PromiseAsyncAwait/asyncawait.js
+++ PromiseAsyncAwait/asyncawait.js
@@ -1,12 +1,11 @@
 //Async and await makes easy to handle promise.
 
-//Example 
+//Example 01
 
 async function getData(){
   const data=await fetch('https://dummyjson.com/products/1')
   const resp=await data.json()
   console.log(resp)
-    return resp
-  
+    return resp 
 }
 getData()
