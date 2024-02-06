7b13e90cfc80200ca1fd3cd4fb14f427b62c7908
Samsu <biosamsuddin@gmail.com>
2024-02-06 15:39:09 +0530

    templateliterals

--- ES6/templateString.js
+++ ES6/templateString.js
@@ -1,2 +1,11 @@
 //Templateliterals
 
+//Template literals are literals delimited with backtick (`) characters, allowing for multi-line strings, string interpolation with embedded expressions, and special constructs called tagged templates.
+`string text`
+
+`string text line 1
+ string text line 2`
+
+`string text ${expression} string text`
+
+tagFunction`string text ${expression} string text`
