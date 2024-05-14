56eb6ff9451161e003461b55ae14181ecb86f30e
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-05-14 12:26:53 +0530

    array coding example

---
+++ OutPut_Based_Question/01_output.js
@@ -0,0 +1,18 @@
+// what is output of the below code
+
+//Example #01  
+let arr = [1, 2, 3, 4, 5, -6, 7];
+arr.length = 0;
+console.log(arr);
+
+
+//Example 02
+const numbers = [11, 25, 31, 23, 33, 18, 200];
+numbers.sort();
+console.log(numbers);
+
+//Example 03
+var of = ["of"];
+for (var of of of) {
+  console.log(of);
+}
