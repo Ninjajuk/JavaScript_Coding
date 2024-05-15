759fee70b8fbd4ac032ecebf2da4716c67fc48aa
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-05-15 11:04:26 +0530

    callback

--- PromiseAsyncAwait/callback.js
+++ PromiseAsyncAwait/callback.js
@@ -2,17 +2,23 @@
 function callbackFunction(name) {
     console.log(`"Hello " ${name}`);
   }
-  
   function outerFunction(callback) {
     let name = prompt("Please enter your name.");
     callback(name);
   }
-  
   outerFunction(callbackFunction);
+  //Example 2 starts here
 
-  
-
-
+  function add(a,b,callback){
+    let sum=a+b
+    callback(sum)
+    
+}
+function displayResult(result){
+    console.log(result)
+}
+add(10,10,displayResult)
+  //Example 2 ends  here
 
 //Synchronous Example 02
 const numbers = [1, 2, 3, 4, 5];
@@ -36,7 +42,7 @@ function simulateAsyncRequest(callback) {
       callback(); // Execute the callback function after the async operation
     }, 2000); // Simulate a 2-second delay
   }
-  
+
   function asyncCallbackFunction() {
     console.log("Callback function executed after async request!");
   }
