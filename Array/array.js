ebc5aeae39d5e2a69a0ca3e175addb156387b3cf
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-05-23 17:08:39 +0530

    table

--- Array/array.js
+++ Array/array.js
@@ -5,7 +5,7 @@ const num=[1,2,3,4,5]
 for (let x in num){
     console.log(x)
 }
-const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
+const fruits1 = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
 
 const similarObjects = [
     { name: 'Alice', age: 30, city: 'Los Angeles', isAdmin: true, isActive: false, dateJoined: new Date('2022-11-10') },
@@ -18,6 +18,12 @@ const similarObjects = [
     { name: 'Hannah', age: 26, city: 'Austin', isAdmin: false, isActive: true, dateJoined: new Date('2022-05-08') },
     { name: 'Ian', age: 34, city: 'Portland', isAdmin: true, isActive: false, dateJoined: new Date('2022-08-30') },
     { name: 'Jessica', age: 33, city: 'Philadelphia', isAdmin: false, isActive: true, dateJoined: new Date('2022-11-28') }
+  ];
+  const fruits = [
+    {name:"apples", quantity:300},
+    {name:"bananas", quantity:500},
+    {name:"oranges", quantity:200},
+    {name:"kiwi", quantity:150}
   ];
   
   console.log(similarObjects);
