08b4ba5d8e9aae8050c6dc3e8c23e0f8f259ad60
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-06-04 12:18:30 +0530

    h

--- ES6/TernaryOperators.js
+++ ES6/TernaryOperators.js
@@ -3,3 +3,6 @@
 const age = 25;
 const isAdult = age >= 18 ? "Adult" : age >= 13 ? "Teenager" : "Child";
 console.log(isAdult);
+
+//Example 02
+
---
+++ Loop/switchcase.js
@@ -0,0 +1,30 @@
+function getProductQuantities(unit) {
+    let quantities;
+  
+    switch (unit) {
+      case 'kg':
+        quantities = ['1 kg', '2 kg', '5 kg', '10 kg'];
+        break;
+      case 'gm':
+        quantities = ['250 gm', '500 gm', '1 kg'];
+        break;
+      case 'ltr':
+        quantities = ['1 ltr', '2 ltr', '5 ltr'];
+        break;
+      case 'item':
+        quantities = ['1 item', '2 items', '3 items', '5 items'];
+        break;
+      default:
+        quantities = ['Unknown unit'];
+    }
+  
+    return quantities;
+  }
+  
+  // Example usage:
+  const productUnit = 'gm';
+  const availableQuantities = getProductQuantities(productUnit);
+  
+  console.log(`Available quantities for unit (${productUnit}):`, availableQuantities);
+  // Output: Available quantities for unit (gm): [ '250 gm', '500 gm', '1 kg' ]
+  
