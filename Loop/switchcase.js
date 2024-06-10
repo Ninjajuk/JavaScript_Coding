d772c1f9bb62daf420075bc10a6558a604aa4ee0
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-06-10 11:48:19 +0530

    or

--- Loop/switchcase.js
+++ Loop/switchcase.js
@@ -1,3 +1,4 @@
+//Example 01
 function getProductQuantities(unit) {
     let quantities;
   
@@ -27,4 +28,40 @@ function getProductQuantities(unit) {
   
   console.log(`Available quantities for unit (${productUnit}):`, availableQuantities);
   // Output: Available quantities for unit (gm): [ '250 gm', '500 gm', '1 kg' ]
-  
+
+
+//Example 02
+const chooseStyle = (status) => {
+  switch (status) {
+    case 'pending':
+      return {
+        colorClass: 'bg-purple-200 text-purple-600',
+        icon: <FaHourglassHalf/> 
+      };
+    case 'dispatched':
+      return {
+        colorClass: 'bg-yellow-200 text-yellow-600',
+        icon: <FaTruck/>
+      };
+    // case 'delivered':
+    //   return {
+    //     colorClass: 'bg-green-200 text-green-600',
+    //     iconClass: <FaBoxOpen />
+    //   };
+    // case 'received':
+    //   return {
+    //     colorClass: 'bg-green-200 text-green-600',
+    //     iconClass: <FaCheckCircle /> 
+    //   };
+    case 'cancelled':
+      return {
+        colorClass: 'bg-red-200 text-red-600',
+        icon: <FaTimesCircle/> 
+      };
+    default:
+      return {
+        colorClass: 'bg-purple-200 text-purple-600',
+        icon: <FaQuestion/>
+      };
+  }
+};
