f698fd4deb2322bc204d04efbebd90b15a82e558
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-06-11 13:15:20 +0530

    ter

--- ES6/TernaryOperators.js
+++ ES6/TernaryOperators.js
@@ -1,7 +1,7 @@
 
 //Example 1
 const age = 25;
-const isAdult = age >= 18 ? "Adult" : age >= 13 ? "Teenager" : "Child";
+const isAdult = age >= 18 ? "Adult" : age >= 13 ? "Teenager" :age >= 5 ?"Child": "Baby";
 console.log(isAdult);
 
 //Example 02
