2ef64ff98e863b0356725f8574fe2ce9e17fc369
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-07-02 12:50:16 +0530

    callstack eventqueue

--- PromiseAsyncAwait/callback.js
+++ PromiseAsyncAwait/callback.js
@@ -1,4 +1,21 @@
-  // Synchronous callback example 1
+//Example of callbaackback queue  ,event loop
+console.log('Start');
+
+setTimeout(() => {
+  console.log('setTimeout');
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log('Promise');
+});
+
+console.log('End');
+//Example of callbaackback queue  ,event loop
+ 
+ 
+ 
+ 
+ // Synchronous callback example 1
 function callbackFunction(name) {
     console.log(`"Hello " ${name}`);
   }
