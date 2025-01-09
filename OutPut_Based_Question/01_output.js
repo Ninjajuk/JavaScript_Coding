311a880d0af30720d1fc6c1fdfc1bb72a3df5455
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2025-01-09 18:54:14 +0530

    outputbased and basic

--- OutPut_Based_Question/01_output.js
+++ OutPut_Based_Question/01_output.js
@@ -1,21 +1,21 @@
 // what is output of the below code
 
 // //Example #01  
-// let arr = [1, 2, 3, 4, 5, -6, 7];
-// arr.length = 0;
-// console.log(arr);
+let arr = [1, 2, 3, 4, 5, -6, 7];
+arr.length = 0;
+console.log(arr);
 
 
 // //Example 02
-// const numbers = [11, 25, 31, 23, 33, 18, 200];
-// numbers.sort();
-// console.log(numbers);
+const numbers = [11, 25, 31, 23, 33, 18, 200];
+numbers.sort();
+console.log(numbers);
 
 // //Example 03
-// var of = ["of"];
-// for (var of of of) {
-//   console.log(of);
-// }
+var of = ["of"];
+for (var of of of) {
+  console.log(of);
+}
 
 
 //Example 01
@@ -132,4 +132,10 @@ console.log(String.prototype.trimLeft.name === "trimStart");
 //Example 15
 console.log(Math.max());
 
-//Example #16 
+//Example #16  on Closures
+
+for (var i = 1; i <= 3; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, i * 1000);
+}
